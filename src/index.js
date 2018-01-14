3f1c1e6ea4d73c0cd765e4ab5cde184bb74cfdae
donaldma <donald.ma@hotmail.ca>
2018-01-13 23:14:39 -0800

    Nav bar (gotta fix mobile collapse)

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './components/app.js'
 import './styles/app.scss'
 import { AppContainer } from 'react-hot-loader'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('main')
-  )
-  
-}
-
-render(App)
-if (module.hot) {
-  module.hot.accept('./components/app', () => { render(App) })
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+  , document.getElementById('main'))
