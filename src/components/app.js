ce91f6675b97152062638b4671e2f1f40277f38c
Felix Lin <blazingfelix@gmail.com>
2018-01-13 17:08:49 -0800

    Added bootstrap to the src

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1 className='f-s-48'>Welcome To Boiler plate</h1>
+        <h1 className='f-s-48'>Welcome, Bootstrap is in!</h1>
+        <Button>Test</Button>
       </div>
     )
   }
