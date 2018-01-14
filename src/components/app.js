a66168c8fa1effeb1ce0132696432030bcac4ffd
donaldma <donald.ma@hotmail.ca>
2018-01-14 00:55:18 -0800

    add profile, courselist

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,23 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 import NavigationBar from './NavigationBar.js'
 import ProfilePanel from './ProfilePanel.js'
-import Calendar from './Calendar.js'
+import CourseList from './CourseList.js'
 
 export default class App extends React.Component {
   render () {
     return (    
 			<div>
 				<NavigationBar />
-				<ProfilePanel />
-				<Calendar />
+				<div className='container home-container'>
+					<div className='row'>
+						<div className='col-xs-12 col-md-5'>
+							<ProfilePanel />
+						</div>
+						<div className='col-xs-12 col-md-7'>
+							<CourseList />
+						</div>
+					</div>
+				</div>
 			</div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './components/app.js'
 import './styles/app.scss'
 import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 ReactDOM.render(
   <BrowserRouter>
-    <div>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </div>
+    </MuiThemeProvider>
   </BrowserRouter>
   , document.getElementById('main'))
